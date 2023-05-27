01cca78be86a0a8c9089a3a4335daa485a8f8d13
fisidi-inc <76191290+joshjgomes@users.noreply.github.com>
2023-05-27 03:20:09 -0400

    changed text

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,9 @@ import { getRandomSeed } from "lib/seeds";
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
-export const appName = "Paint by Text";
-export const appSubtitle = "Edit your photos using written instructions, with the help of an AI.";
-export const appMetaDescription = "Edit your photos using written instructions, with the help of an AI.";
+export const appName = "Artify";
+export const appSubtitle = "Forget using a mouse to edit your photos use your words instead";
+export const appMetaDescription = "By Fisidi Inc.";
 
 export default function Home() {
   const [events, setEvents] = useState([]);
